f368233b562b34897e34a54512c7c7cebd7533a8
Esteban Román <eromancastellanos@educa.madrid.org>
2018-10-31 20:37:36 +0100

    Añadidos nuevos campos a comparte+

--- leaflet/comparte/js/comparte.js
+++ leaflet/comparte/js/comparte.js
@@ -5,25 +5,15 @@ var myLayer;
 var estadoAmbito = "all";
 var estadoEtapa = "all";
 // esta función solo es necesaria por que reseteamos el mapa completo y no solo las marcas
-var pruebas = new L.Icon({
-    iconUrl: 'https://iot.educa.madrid.org/imagenes/autoescuela-peque.png',
+  var smallIcon = new L.Icon({
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon.png',
     iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon-2x.png',
-    iconSize:    [20, 25],
-    iconAnchor:  [0, 0],
+    iconSize:    [25, 41],
+    iconAnchor:  [12, 41],
     popupAnchor: [1, -34],
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
     shadowSize:  [41, 41]
-});
-  
-var nube = new L.Icon({
-    iconUrl: 'https://iot.educa.madrid.org/imagenes/crif_p.png',
-    iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon-2x.png',
-    iconSize:    [45, 45],
-    iconAnchor:  [0, 0],
-    popupAnchor: [1, -34],
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-    shadowSize:  [41, 41]
-});
+  });
 
 function crearMapa(){
     map = L.map('map').setView([40.505, -3.79], 11);
@@ -56,9 +46,14 @@ function onEachFeature(feature, layer) {
 		$( "#leyenda" ).show( "fast" );
         $( "#aviso" ).hide("fast");
 		document.getElementById("centro").innerHTML=feature.properties.centro;
-		document.getElementById("ambito").innerHTML=feature.properties.ambito;
-		document.getElementById("etapa").innerHTML=feature.properties.etapa;
-		document.getElementById("descripcion").innerHTML=feature.properties.descripcion;
+		document.getElementById("ambito").innerHTML="Ámbito: " + feature.properties.ambito;
+		document.getElementById("etapa").innerHTML="Etapa: " + feature.properties.etapa;
+		document.getElementById("descripcion").innerHTML="Descripción: "+feature.properties.descripcion;
+		if (feature.properties.enlace = "no"){
+			document.getElementById("enlace").innerHTML="No tiene enlace";		
+		}else{
+			document.getElementById("enlace").innerHTML="<a target=\"_blank\" href=\" " + feature.properties.enlace + "\">enlace</a>";}
+		
 	});
 }
 
@@ -69,9 +64,9 @@ function representar(data, ambito, etapa){
 		pointToLayer: function(feature, latlng) {
 			var icono;
 			if (feature.properties.tags == "pruebas"){
-				icono = pruebas;
+				icono = smallIcon;
                 	}else{
-                    		icono = nube;
+                    		icono = smallIcon;
                 	}
        			return L.marker(latlng, {
 				icon: icono
@@ -89,7 +84,7 @@ function representar(data, ambito, etapa){
 }
 
 //PARSEAMOS EL GEOJSON QUE PROVIENE LA URL UTILIZANDO AJAX Y LLAMAMOS A LA FUNCIÓN QUE REPRESENTA PASÁNDOLE EL DATA
-$.getJSON("https://script.googleusercontent.com/macros/echo?user_content_key=SWFc-3sQi9sna9TMAI8AnPvdxTmKaZwmqpwWBMR6hckowR5b-gqpZXBy96WSJ2ISbSYJO9REaUv9P6cJaZgKixyojPROkPMIm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnI7bJHnJ-eJr0gWxK_o2antMoO5Nf87R3jJY8rc6IsQGfy6iRLWzR60n-OJs8rPC_UW-dnclfdIe&lib=MvDgdm_fmzszgEhwAQMC-TY62uAqfJ8lq", function(data) {
+$.getJSON("https://script.googleusercontent.com/macros/echo?user_content_key=HaoObimc-RmuCU4m8YMnpL3F9vP40-E_vx5MNhCAp1-c-UdHvM42mIcut0lKrUe21P5-xoDfDsYPcr7SSzBEdSIdTxuSzPndm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnI7bJHnJ-eJr0gWxK_o2antMoO5Nf87R3jJY8rc6IsQGfy6iRLWzR60n-OJs8rPC_UW-dnclfdIe&lib=MvDgdm_fmzszgEhwAQMC-TY62uAqfJ8lq", function(data) {
 	representar(data, "all","all");
 	datos = data;
 	
